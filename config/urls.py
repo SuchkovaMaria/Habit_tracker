f7be12c83780dd6ae99bcd3001b4d5f2303b7902
Mariya_Suchkova <mashakraxmal@gmail.com>
2026-07-02 20:05:00 +0300

    init commit

---
+++ config/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+
+from config import settings
+
+urlpatterns = [
+      path("admin/", admin.site.urls),
+      path("habits/", include("habits.urls", namespace="habits")),
+      path("users/", include("users.urls", namespace="users")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
